f512f9ab65bf7a7f3a24ca81361028258629b1f7
Aarjion <lfybtkm37@mail.ru>
2020-10-23 13:42:59 +0500

    1

---
+++ Лаба3/Bipit3/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bipit3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+        {
+            //string path = @"Server = localhost\SQLEXPRESS; Database = Библиотека; Trusted_Connection = True";
+            string path = @"Data Source=Library00001.mssql.somee.com;Initial Catalog=Library00001;User Id=D98512416_SQLLogin_1;Password=5vem21popc";
+            if (Menu1.SelectedValue.Contains("Читатель"))
+            {
+                string query = "SELECT * FROM Читатель";
+                DataSet dataSet = new DataSet();
+                using (SqlConnection connection = new SqlConnection(path))
+                {
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connection);
+                    sqlDataAdapter.Fill(dataSet, "Читатель");
+                    GridView1.DataSource = dataSet.Tables["Читатель"];
+                    GridView1.DataBind();
+                }
+            }
+            if (Menu1.SelectedValue.Contains("Книги"))
+            {
+                string query = "SELECT * FROM Книги";
+                DataSet dataSet = new DataSet();
+                using (SqlConnection connection = new SqlConnection(path))
+                {
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connection);
+                    sqlDataAdapter.Fill(dataSet, "Книги");
+                    GridView1.DataSource = dataSet.Tables["Книги"];
+                    GridView1.DataBind();
+                }
+            }
+            if (Menu1.SelectedValue.Contains("Выдача"))
+            {
+                string query = "SELECT * FROM Выдача";
+                DataSet dataSet = new DataSet();
+                using (SqlConnection connection = new SqlConnection(path))
+                {
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connection);
+                    sqlDataAdapter.Fill(dataSet, "Выдача");
+                    GridView1.DataSource = dataSet.Tables["Выдача"];
+                    GridView1.DataBind();
+                }
+            }
+        }
+    }
+}
---
+++ Лаба3/Bipit3/obj/Debug/AspnetCompileMerge/Source/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Bipit3.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Menu ID="Menu1" runat="server" OnMenuItemClick="Menu1_MenuItemClick" StaticSubMenuIndent="10px" style="padding:10px; height: 250px; width: 100px;" BackColor="#B5C7DE" DynamicHorizontalOffset="2" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#284E98">
+        <DynamicHoverStyle BackColor="#284E98" ForeColor="White" />
+        <DynamicMenuItemStyle HorizontalPadding="5px" VerticalPadding="2px" />
+        <DynamicMenuStyle BackColor="#B5C7DE" />
+        <DynamicSelectedStyle BackColor="#507CD1" />
+        <DynamicItemTemplate>
+            <%# Eval("Text") %>
+        </DynamicItemTemplate>
+        <Items>
+            <asp:MenuItem Text="Читатель" Value="Читатель"></asp:MenuItem>
+            <asp:MenuItem Text="Книги" Value="Книги"></asp:MenuItem>
+            <asp:MenuItem Text="Выдача" Value="Выдача"></asp:MenuItem>
+        </Items>
+        <StaticHoverStyle BackColor="#284E98" ForeColor="White" />
+        <StaticMenuItemStyle HorizontalPadding="5px" VerticalPadding="2px" />
+        <StaticSelectedStyle BackColor="#507CD1" />
+    </asp:Menu>
+    <asp:GridView ID="GridView1" runat="server" CellPadding="5" style="margin-left: 150px; margin-top: -250px;">
+    </asp:GridView>
+    &nbsp;&nbsp;&nbsp; 
+    </asp:Content>
